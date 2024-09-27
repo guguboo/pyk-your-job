6fd544cc64b8f562e62374ab9f49a3fa06d51e30
Vico Pratama <135086306+guguboo@users.noreply.github.com>
2024-09-28 02:58:42 +0700

    jobs half done

--- src/dummy_project_frontend/src/components/User/Jobs.jsx
+++ src/dummy_project_frontend/src/components/User/Jobs.jsx
@@ -5,8 +5,16 @@ import { jobs_backend } from 'declarations/jobs_backend';
 export default function Jobs() {
     const [jobs, setJobs] = useState([]);
     const [newJob, setNewJob] = useState('');
+    const [desc, setDesc] = useState('');
+    const [creatorId, setCreatorId] = useState('');
+    const [company, setCompany] = useState('');
+    const [duration, setDuration] = useState(0);
 
-    useEffect(() => {
+    
+
+    useEffect(async () => {
+        const id = await jobs_backend.whoami()
+        setCreatorId(id) //insert internet id here
         fetchJobs();
     }, []);
 
@@ -14,6 +22,7 @@ export default function Jobs() {
     const fetchJobs = async () => {
         try {
         const fetchedJobs = await jobs_backend.getJobs();
+        print(fetchedJobs)
         setJobs(fetchedJobs);
         } catch (error) {
         console.error('Error fetching jobs:', error);
@@ -23,10 +32,14 @@ export default function Jobs() {
     // This function is similar to making a POST request
     const handleAddJob = async (e) => {
         e.preventDefault();
-        if (!newJob.trim()) return;
+        if (!newJob.trim() && !desc.trim() && !creatorId.trim() && !company.trim()) return;
         try {
-        await jobs_backend.addJob(newJob);
+        await jobs_backend.addJob(newJob, creatorId, company, Number(duration), desc);
         setNewJob('');
+        setDesc('');
+        setCreatorId('');
+        setCompany('');
+        setDuration(0);
         fetchJobs();
         } catch (error) {
         console.error('Error adding job:', error);
@@ -34,22 +47,58 @@ export default function Jobs() {
     };
 
     return (
-        <div className="job-app">
-        <h1>Job List</h1>
-        <form onSubmit={handleAddJob}>
-            <input
-            type="text"
-            value={newJob}
-            onChange={(e) => setNewJob(e.target.value)}
-            placeholder="Add a new job"
-            />
-            <button type="submit">Add Job</button>
-        </form>
-        <ul>
-            {jobs.map((job) => (
-            <li key={job.id}>{job.text}</li>
-            ))}
-        </ul>
+        <div className='w-full p-[10%]'>
+
+            <div className="job-app">
+            <div className='text-4xl font-black'>Available Jobs:</div>
+            <form onSubmit={handleAddJob} className='flex flex-col gap-3 my-10'>
+                <label>Job Title</label>
+                <input
+                name="title"
+                type="text"
+                className="text-[#1e1d1d] px-3 mb-4 rounded-lg w-[30%]"
+                value={newJob}
+                onChange={(e) => setNewJob(e.target.value)}
+                placeholder="Add a new job"
+                />
+                <label>Description</label>
+                <input
+                type="text"
+                className="text-[#0e0e0e] h-28 px-3 mb-4 rounded-lg w-[30%]"
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+                placeholder="Enter Job Description"
+                />
+                <label>Company Name</label>
+                <input
+                type="text"
+                className="text-[#0e0e0e] px-3 mb-4 rounded-lg w-[30%]"
+                value={company}
+                onChange={(e) => setCompany(e.target.value)}
+                placeholder="Enter Company Name"
+                />
+                <label>Registration Duration</label>
+                <input
+                type="number"
+                className="text-[#0e0e0e] px-3 mb-4 rounded-lg w-[30%]"
+                value={duration}
+                onChange={(e) => setDuration(e.target.value)}
+                placeholder="Enter Duration in Days From Now"
+                />
+                <button type="submit" className='bg-white text-[#0e0e0e] w-[10%] rounded-lg mt-4'>Add Job</button>
+            </form>
+            <div className='text-4xl font-black mb-10'>Jobs List</div>
+            <div className='grid grid-cols-4 gap-5'>
+                {jobs.map((job) => (
+                    <div key={job.id} className='p-10 bg-white bg-opacity-10 rounded-xl border border-white flex flex-col gap-3'>
+                        <div key={job.id} className='font-bold text-4xl'>{job.jobTitle}</div>   
+                        <div key={job.id} className='font-bold text-lg'>{job.company}</div>
+                        <div key={job.id} className='font-light text-lg'>{job.description}</div>
+                        <div key={job.id} className='font-bold text-lg'>Deadline: {job.registrationEndDate} in Nanosecond</div>
+                    </div>
+                ))}
+            </div>
+            </div>
         </div>
     );
 }
--- src/jobs_backend/main.mo
+++ src/jobs_backend/main.mo
@@ -1,14 +1,38 @@
 import Array "mo:base/Array";
 import Text "mo:base/Text";
+import Time "mo:base/Time";
+import Nat "mo:base/Nat";
+import Int "mo:base/Int";
+import Trie "mo:base/Trie";
+import Debug "mo:base/Debug";
+import Principal "mo:base/Principal";
+
+
 
 actor {
+    //CLASS FOR JOB
+    var oneDay:Nat = 24 * 60 * 60 * 1_000_000_000;
+    type Trie<K, V> = Trie.Trie<K, V>;
+    type Key<K> = Trie.Key<K>;
     type Job = {
         id: Nat;
-        text: Text;
+        creatorId: Text;
         completed: Bool;
+        jobTitle: Text;
+        company: Text;
+        registrationEndDate: Int;
+        description: Text;
     };
 
-    var jobs: [Job] = [];
+    type User = {
+        id: Nat;
+        internetId: Text;
+        applied_jobs: [Job];
+        application_date: [Nat];
+    };
+
+    stable var jobs: [Job] = [];
+    stable let users: Trie<Text, [Job]> = Trie.empty();
     var nextId: Nat = 0;
 
     // This function acts like a GET request
@@ -17,14 +41,24 @@ actor {
     };
 
     // This function acts like a POST request
-    public func addJob(text: Text) : async Nat {
+    public func addJob(jobTitle: Text, creatorId: Text, company: Text, registrationDays: Int, description: Text) : async Nat {
+        let now: Int = Time.now();
         let job: Job = {
             id = nextId;
-            text = text;
+            creatorId = creatorId;
             completed = false;
+            jobTitle = jobTitle;
+            company = company;
+            registrationEndDate = now + (oneDay * registrationDays);
+            description = description;
         };
         jobs := Array.append(jobs, [job]);
         nextId += 1;
         nextId - 1
+    };
+
+    public shared query (msg) func whoami() : async Text {
+    Debug.print("Caller: " # Principal.toText(msg.caller));
+    Principal.toText(msg.caller)
     };
 }
