669651d1ffd8130998a465bf151a4dfa7207a4d9
Vico Pratama <135086306+guguboo@users.noreply.github.com>
2024-09-28 12:19:43 +0700

    back to non-parallax

--- src/dummy_project_frontend/src/components/Talents.jsx
+++ src/dummy_project_frontend/src/components/Talents.jsx
@@ -5,18 +5,18 @@ function Talents(){
                 <div className="w-[80rem] mx-auto flex flex-col">
                     <div className="text-5xl font-bold text-center mb-[5%]">Pick Your Talents Now</div>
                     <div className="grid grid-cols-2 justify-center">
-                        <div className="transition-opacity group flex flex-col px-[15%] py-[20%] m-[5%] ml-0 rounded-[3rem] border justify-between gap-24 font-semibold relative bg-no-repeat bg-cover hover:bg-[url('/img/talents/bg.png')]">
+                        <a href="/jobs" className="transition-opacity group flex flex-col px-[15%] py-[20%] m-[5%] ml-0 rounded-[3rem] border justify-between gap-24 font-semibold relative bg-no-repeat bg-cover hover:bg-[url('/img/talents/bg.png')]">
                             <div className="group-hover:text-[#0e0e0e] text-9xl">Devs</div>
                             <div className="group-hover:text-[#0e0e0e] font-light text-lg w-[90%]">Find skilled developers for your projects. From web development to blockchain expertise, we have the right talent to bring your ideas to life.</div>
                             <img src="/img/talents/black.png" className="absolute top-20 right-20 w-[13%] group-hover:hidden"/>
                             <img src="/img/talents/white.png" className=" hidden absolute top-20 right-20 w-[13%] group-hover:block"/>
-                        </div>
-                        <div className="transition-opacity group flex flex-col px-[15%] py-[20%] m-[5%] mr-0 rounded-[3rem] border justify-between gap-24 font-semibold relative bg-no-repeat bg-cover hover:bg-[url('/img/talents/bg.png')]">
+                        </a>
+                        <a href="/jobs" className="transition-opacity group flex flex-col px-[15%] py-[20%] m-[5%] mr-0 rounded-[3rem] border justify-between gap-24 font-semibold relative bg-no-repeat bg-cover hover:bg-[url('/img/talents/bg.png')]">
                             <div className="group-hover:text-[#0e0e0e] text-9xl">Non<br/>Devs</div>
                             <div className="group-hover:text-[#0e0e0e] font-light text-lg w-[90%]">Discover a wide range of professionals for marketing, design, writing, and more. Get the expertise you need to grow your business.</div>
                             <img src="/img/talents/black.png" className="absolute top-20 right-20 w-[13%]"/>
                             <img src="/img/talents/white.png" className=" hidden absolute top-20 right-20 w-[13%] group-hover:block"/>
-                        </div>
+                        </a>
                     </div>
                 </div>
             </div>
--- src/dummy_project_frontend/src/pages/Home.jsx
+++ src/dummy_project_frontend/src/pages/Home.jsx
@@ -8,16 +8,10 @@ import Footer from '../components/Footer';
 
 const Home = () => {
   return (
-    <Parallax pages={3}>
     <div className='w-[100vw] flex flex-col gap-40 justify-start'>
-      <ParallaxLayer speed={0.7}>
         <img src="/img/landing/cube1.png" id="cube1" className="w-[60%] absolute top-[7%] left-[60%] z-0"/>
-      </ParallaxLayer>
-      <ParallaxLayer speed={0.05}>
         <img src="/img/landing/cube2.png" id="cube2" className="w-[30%] absolute top-[5%] left-[-13%] z-0"/>
-      </ParallaxLayer>
 
-      <ParallaxLayer speed={0} factor={3}>
         <Navbar />
           <div id="header">
             <Landing />
@@ -29,9 +23,7 @@ const Home = () => {
             <Talents />
           </div>
         <Footer />
-      </ParallaxLayer>
     </div>
-    </Parallax>
   );
 }
 
